4987d062962d130f193b34a8253e5ea0791533aa
Sreelakshmi <sreelakshmimanojkumar2006@gmail.com>
2026-07-26 08:50:25 +0530

    Cloudinary added

--- BACKEND/config/cloudinary.js
+++ BACKEND/config/cloudinary.js
@@ -5,5 +5,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log("Cloudinary configured successfully!");
+
 
 module.exports = cloudinary;
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const app = require("./app");
 const connectDB = require("./config/db");
+const cloudinary = require("./config/cloudinary"); // Import Cloudinary
 
 // Connect to MongoDB
 connectDB();
